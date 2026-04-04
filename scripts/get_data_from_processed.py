6ec233cf20db8e3095f057b2bb0598a5bd8b762f
19406 <hoaikhiem2k4@gmail.com>
2026-04-04 15:27:05 +0700

    create get data file

---
+++ scripts/get_data_from_processed.py
@@ -0,0 +1,45 @@
+from pyspark.sql import SparkSession
+# Load .env
+from dotenv import load_dotenv
+load_dotenv()
+import os
+
+def get_spark_session():
+    storage_account = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+    account_key = os.getenv("AZURE_STORAGE_ACCOUNT_KEY")
+    spark = (
+        SparkSession.builder
+        .master("local[*]")
+        .appName("VN30 Stock Analysis")
+        .config("spark.jars.packages", "org.apache.hadoop:hadoop-azure:3.4.1")
+        .config("spark.sql.legacy.parquet.nanosAsLong", "true")
+        .getOrCreate()
+    )
+    
+    spark.conf.set(
+        f"fs.azure.account.auth.type.{storage_account}.dfs.core.windows.net",
+        "SharedKey"
+    )
+    spark.conf.set(
+        f"fs.azure.account.key.{storage_account}.dfs.core.windows.net",
+        account_key
+    )
+
+    spark.conf.set("spark.sql.session.timeZone", "Asia/Ho_Chi_Minh")
+    
+    return spark
+
+storage_account = os.getenv("AZURE_STORAGE_ACCOUNT_NAME")
+BASE_PATH_PROCESSED = f"abfss://processed@{storage_account}.dfs.core.windows.net"
+
+spark = get_spark_session()
+df = (
+    spark.read
+    .option("recursiveFileLookup", "true")
+    .option("pathGlobFilter", "*.parquet")
+    .parquet(
+        f"{BASE_PATH_PROCESSED}/2026"
+    )
+)
+
+df.show(truncate=False)
